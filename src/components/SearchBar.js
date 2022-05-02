2b457cdf67d3350fd2742becab22f94425b4aeb3
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-05-02 16:32:40 -0300

    :construction: feat: implementando o componente SearchBar.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,12 @@ import React from 'react';
 function SearchBar() {
   return (
     <form>
-      <input type="text" />
-      <input type="radio" />
-      <input type="radio" />
-      <input type="radio" />
+      <input data-testid="exec-search-btn" type="text" placeholder="" />
+      <fieldset>
+        <input data-testid="ingredient-search-radio" type="radio" />
+        <input data-testid="name-search-radio" type="radio" />
+        <input data-testid="first-letter-search-radio" type="radio" />
+      </fieldset>
       <button type="button">Search</button>
     </form>
   );
