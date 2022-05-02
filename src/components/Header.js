ee5d8737edca4a738d9e4d1a4ce6cecfdf286a86
LDantasA <leonardo.dantas0@gmail.com>
2022-05-02 16:13:21 -0300

    Prop title adicionada iconButton

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,21 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import IconButton from './IconButton';
 import SearchIconButton from './SearchIconButton';
 import '../sass/components/Header.css';
 
-function Header() {
-  const { pathname } = useLocation();
-  const titles = [
-    ['/foods', 'Foods'],
-    ['/drinks', 'Drinks'],
-    ['/explore', 'Explore'],
-    ['/explore/foods', 'Explore Foods'],
-    ['/explore', 'Explore Drinks'],
-    ['/explore/foods/ingredients', 'Explore Ingredients'],
-    ['/explore/drinks/ingredients', 'Explore Ingredients'],
-    ['/explore/foods/nationalities', 'Explore Nationalities'],
-    ['/profile', 'Profile'],
-    ['/done-recipes', 'Done Recipes'],
-    ['/favorite-recipes', 'Favorite Recipes'],
-  ];
-
+function Header({ title }) {
   return (
     <header className="Header">
       <IconButton route="/profile" data-testid="profile-top-btn" />
-      <h1 data-testid="page-title">{ titles.find((item) => item[0] === pathname)[1] }</h1>
+      <h1 data-testid="page-title">{ title }</h1>
       <SearchIconButton data-testid="search-top-btn" />
     </header>
   );
 }
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default Header;
