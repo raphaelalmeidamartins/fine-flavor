d57d6479d340478345df5eb99b9d33ec81ae7b2b
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-05-03 08:03:01 -0300

    :construction: feat: lógica do botão de categoria em andamento

--- src/components/CategoryButton.js
+++ src/components/CategoryButton.js
@@ -1,9 +1,16 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-function CategoryButton() {
+function CategoryButton({ categoryName }) {
   return (
-    <button type="button">Category</button>
+    <button type="button" data-testid={ `${categoryName}-category-filter` }>
+      {categoryName}
+    </button>
   );
 }
+
+CategoryButton.propTypes = {
+  categoryName: PropTypes.string.isRequired,
+};
 
 export default CategoryButton;
