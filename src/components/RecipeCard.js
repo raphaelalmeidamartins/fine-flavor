b3acca70f69bbcdfe3cd19560d42d5579bf15d06
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-05-03 07:41:01 -0300

    :sparkles: feat: criei componente RecipeCard.

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,12 +1,20 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import '../sass/components/RecipeCard.css';
 
-function RecipeCard() {
+function RecipeCard({ title, thumbnail, index }) {
   return (
-    <section>
-      <img src="" alt="" />
-      <h3>Titulo da comida</h3>
+    <section data-testid={ `${index}-recipe-card` } className="RecipeCard">
+      <img data-testid={ `${index}-card-img` } src={ thumbnail } alt={ title } />
+      <h3 data-testid={ `${index}-card-name` }>{ title }</h3>
     </section>
   );
 }
+
+RecipeCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  thumbnail: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+};
 
 export default RecipeCard;
