316d139b4f841dfc6ae8f5eb3ba69882a2a251d1
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-05-03 07:56:39 -0300

    :sparkles: feat: o componente RecipeCard agora redireciona para a página da receita.

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,17 +1,28 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import '../sass/components/RecipeCard.css';
 
-function RecipeCard({ title, thumbnail, index }) {
+function RecipeCard({ id, mealOrDrink, title, thumbnail, index }) {
+  const history = useHistory();
+  const route = mealOrDrink === 'Meal' ? '/food' : '/drink';
+
   return (
-    <section data-testid={ `${index}-recipe-card` } className="RecipeCard">
+    <button
+      data-testid={ `${index}-recipe-card` }
+      type="button"
+      className="RecipeCard"
+      onClick={ () => history.push(`${route}/${id}`) }
+    >
       <img data-testid={ `${index}-card-img` } src={ thumbnail } alt={ title } />
-      <h3 data-testid={ `${index}-card-name` }>{ title }</h3>
-    </section>
+      <h3 data-testid={ `${index}-card-name` }>{title}</h3>
+    </button>
   );
 }
 
 RecipeCard.propTypes = {
+  id: PropTypes.string.isRequired,
+  mealOrDrink: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   thumbnail: PropTypes.string.isRequired,
   index: PropTypes.number.isRequired,
