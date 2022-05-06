674003bfed8c4bd1ab38c0824f669ce520616605
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-05-05 21:14:54 -0300

    :heavy_plus_sing: build: instalei dependência.

--- src/components/RecommendationsCarousel.js
+++ src/components/RecommendationsCarousel.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import Slider from 'react-slick';
-import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Slider from 'react-slick';
 import RecipeCard from './RecipeCard';
 
 function RecommendationsCarousel({ type }) {
