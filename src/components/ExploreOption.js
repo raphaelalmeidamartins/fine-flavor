d6359ea5dd364d534c964b0d003d953a54f8c45e
LDantasA <leonardo.dantas0@gmail.com>
2022-05-06 19:01:39 -0300

    :sparkles: feat: Páginas explore funcionando (quase)

--- src/components/ExploreOption.js
+++ src/components/ExploreOption.js
@@ -1,9 +1,34 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-function ExploreOption() {
+function ExploreOption({ text, route, image, dataTestId, imageTestId, nameTestId }) {
+  const history = useHistory();
   return (
-    <button type="button">Explore Option</button>
+    <button
+      type="button"
+      onClick={ () => history.push(route) }
+      data-testid={ dataTestId }
+    >
+      {image ? <img src={ image } alt={ text } data-testid={ imageTestId } /> : null}
+      <p { ...nameTestId ? { 'data-testid': nameTestId } : {} }>{ text }</p>
+    </button>
   );
 }
+
+ExploreOption.defaultProps = {
+  image: '',
+  imageTestId: '',
+  nameTestId: '',
+};
+
+ExploreOption.propTypes = {
+  text: PropTypes.string.isRequired,
+  route: PropTypes.string.isRequired,
+  image: PropTypes.string,
+  dataTestId: PropTypes.string.isRequired,
+  imageTestId: PropTypes.string,
+  nameTestId: PropTypes.string,
+};
 
 export default ExploreOption;
