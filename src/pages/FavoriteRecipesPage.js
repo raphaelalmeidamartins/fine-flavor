29c7a9f49353c1da0c79ad69c6aa2c5ad925db67
LDantasA <leonardo.dantas0@gmail.com>
2022-05-07 19:57:54 -0300

    :sparkles: feat: tela de receitas favoritas

--- src/pages/FavoriteRecipesPage.js
+++ src/pages/FavoriteRecipesPage.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import CategoryButton from '../components/CategoryButton';
-import FavoriteRecipecard from '../components/FavoriteRecipeCard';
 import Header from '../components/Header';
+import FavoriteRecipeCard from '../components/FavoriteRecipeCard';
 
 function FavoriteRecipesPage() {
+  const { favoriteRecipes } = useSelector((state) => state);
+  const [filter, setFilter] = useState('All');
+
   return (
     <div>
       <Header title="Favorite Recipes" />
+      <section>
+        <CategoryButton
+          categoryName="All"
+          dataTestId={ () => 'filter-by-all-btn' }
+          handleClick={ () => setFilter('All') }
+        />
+        <CategoryButton
+          categoryName="Foods"
+          dataTestId={ () => 'filter-by-food-btn' }
+          handleClick={ () => setFilter('food') }
+        />
+        <CategoryButton
+          categoryName="Drinks"
+          dataTestId={ () => 'filter-by-drink-btn' }
+          handleClick={ () => setFilter('drink') }
+        />
+      </section>
       <main>
-        <FavoriteRecipecard />
-        <CategoryButton />
+        {filter === 'All' && favoriteRecipes?.length
+          ? favoriteRecipes.map((recipe, index) => (
+            <FavoriteRecipeCard key={ recipe.id } index={ index } { ...recipe } />
+          ))
+          : favoriteRecipes.filter(({ type }) => type === filter).map((recipe, index) => (
+            <FavoriteRecipeCard key={ recipe.id } index={ index } { ...recipe } />
+          ))}
       </main>
     </div>
   );
