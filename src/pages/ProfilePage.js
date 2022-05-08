f35a744cde543179cd27f110f5e72ae478eda487
Danielly Olimpio <danielly.fundcheller@gmail.com>
2022-05-07 21:31:40 -0300

    :heavy: feat: Implementaçao da pagina de perfil

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import NavBar from '../components/NavBar';
 
 function ProfilePage() {
+  const email = useSelector((state) => state.user.email);
+  const history = useHistory();
+
   return (
     <div>
       <Header title="Profile" />
+      <h2
+        data-testid="profile-email"
+      >
+        {email}
+      </h2>
+      <button
+        data-testid="profile-done-btn"
+        type="button"
+        onClick={ () => history.push('/done-recipes') }
+      >
+        Done Recipes
+
+      </button>
+      <button
+        data-testid="profile-favorite-btn"
+        type="button"
+        onClick={ () => history.push('/favorite-recipes') }
+      >
+        Favorite Recipes
+
+      </button>
+      <button
+        data-testid="profile-logout-btn"
+        type="button"
+        onClick={ () => {
+          localStorage.clear();
+          history.push('/');
+        } }
+      >
+        Logout
+
+      </button>
+
       <NavBar />
     </div>
   );
