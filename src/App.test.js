57468ec18523b63a662b8a90f2280ebd82d83442
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-05-08 20:01:24 -0300

    :construction: build: criei helper renderWithRouterAndRedux.

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('Farewell, front-end', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
-});
---
+++ src/tests/helpers/index.js
@@ -0,0 +1,24 @@
+import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import rootReducer from '../../redux/reducers';
+
+function renderWithRouterAndRedux(
+  component,
+  { initialState, store = createStore(rootReducer, initialState) } = {},
+) {
+  const history = createMemoryHistory();
+  return {
+    ...render(
+      <Provider store={ store }>
+        <Router history={ history }>{component}</Router>
+      </Provider>,
+    ),
+    history,
+    store,
+  };
+}
+
+export default renderWithRouterAndRedux;
