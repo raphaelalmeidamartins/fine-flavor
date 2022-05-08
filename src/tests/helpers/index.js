05b0bb352fca2505fbb12a476dca4b772bbff030
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-05-08 20:41:21 -0300

    :construction: build: implementei redux toolkit para simplificar store.

--- src/tests/helpers/index.js
+++ src/tests/helpers/index.js
@@ -1,15 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit';
 import { render } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import rootReducer from '../../redux/reducers';
-import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
 
 function renderWithRouterAndRedux(
   component,
-  { initialState, store = createStore(rootReducer, initialState) } = {},
+  {
+    initialState,
+    store = configureStore({
+      reducer: rootReducer,
+      preloadedState: initialState,
+    }),
+  } = {},
 ) {
   const history = createMemoryHistory();
   return {
