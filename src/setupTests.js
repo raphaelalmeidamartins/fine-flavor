b42036cc10c49e350c5a86dd8c6a125e7483c0b4
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-05-10 12:08:34 -0300

    :white_check_mark: test: removi comentários nos testes 02_MainPage.spec.js.

--- src/setupTests.js
+++ src/setupTests.js
@@ -10,3 +10,8 @@ window.matchMedia = window.matchMedia
     addListener: () => {},
     removeListener: () => {},
   }));
+
+/* source: https://github.com/nickcolley/jest-axe/issues/147#issuecomment-758804533 */
+
+const { getComputedStyle } = window;
+window.getComputedStyle = (elt) => getComputedStyle(elt);
