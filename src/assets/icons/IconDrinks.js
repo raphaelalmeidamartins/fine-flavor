dd0e0ea0242abaaa053f1296fd95606517631155
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-05-11 00:54:39 -0300

    :lipstick: adicionei viewBox nos ícones.

--- src/assets/icons/IconDrinks.js
+++ src/assets/icons/IconDrinks.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function IconDrinks() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48">
+    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48">
       <path d="M12 42v-3h10.5V26.8L6 9V6h36v3L25.5 26.8V39H36v3zm2.05-28.75h19.9L38.1 9.2H9.9zM24 24.05l7.2-7.8H16.8zm0 0l-7.2-7.8h14.4z" />
     </svg>
   );
