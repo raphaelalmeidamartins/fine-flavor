e116a61747cb9257fe4aec8038b8bd1cfc3766d3
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-05-12 13:25:19 -0300

    :lipstick: style: estilizei página de perfil.

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -3,39 +3,42 @@ import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import NavBar from '../components/NavBar';
+import '../sass/pages/ProfilePage.css';
 
 function ProfilePage() {
   const email = useSelector((state) => state.user.email);
   const history = useHistory();
 
   return (
-    <div>
+    <div className="ProfilePage">
       <Header title="Profile" back />
-      <h2 data-testid="profile-email">{email}</h2>
-      <button
-        data-testid="profile-done-btn"
-        type="button"
-        onClick={ () => history.push('/done-recipes') }
-      >
-        Done Recipes
-      </button>
-      <button
-        data-testid="profile-favorite-btn"
-        type="button"
-        onClick={ () => history.push('/favorite-recipes') }
-      >
-        Favorite Recipes
-      </button>
-      <button
-        data-testid="profile-logout-btn"
-        type="button"
-        onClick={ () => {
-          localStorage.clear();
-          history.push('/login');
-        } }
-      >
-        Logout
-      </button>
+      <main>
+        <h2 data-testid="profile-email">{email}</h2>
+        <button
+          data-testid="profile-done-btn"
+          type="button"
+          onClick={ () => history.push('/done-recipes') }
+        >
+          Done Recipes
+        </button>
+        <button
+          data-testid="profile-favorite-btn"
+          type="button"
+          onClick={ () => history.push('/favorite-recipes') }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          data-testid="profile-logout-btn"
+          type="button"
+          onClick={ () => {
+            localStorage.clear();
+            history.push('/login');
+          } }
+        >
+          Logout
+        </button>
+      </main>
       <NavBar />
     </div>
   );
